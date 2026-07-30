70258a62958118393299c6e4603e511613d6e066
minhnhuthihi <klemin3005@gmail.com>
2026-07-30 15:25:56 +0700

    update mailer

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth:{
+    family: 4,
+    auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
     },
